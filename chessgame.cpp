399f90385a584091fdbbdafb307ac73c7cc71e3f
gaurmand <gaurav_amwan@hotmail.com>
2021-01-06 11:24:59 -0800

    Set correct initial chess piece positions

--- chessgame.cpp
+++ chessgame.cpp
@@ -229,7 +229,7 @@ void ChessGame::setInitialGameState()
     clearBoard();
 
     //set board and game state
-    setGameState("rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2");
+    setGameState("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
 
     numAvailableMoves = 0;
     _isCheck = false;
